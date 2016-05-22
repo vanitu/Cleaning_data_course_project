ec52a0c9ef426b4b796e0ee379bd0e45229ab23f
Vanitu <no@vanitu.ru>
2016-05-22 19:33:38 +0300

    Commit datasets and codebook

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # Cleaning_data_course_project
+
+Tidy Data Set Preparation
+=================
+
+Project represents preparing tidy data set from Human Activity Recognition Using Smartphones Dataset. 
+
+Source DataSet and its description can be found at https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+##Tidy Data sets prepared:
+
+- 'tidy_data/tidy_set.csv': Each row represents an observation window with labeled activity and subject. Each row includes mean and standard deviations for each measurement from a source dataset. 
+
+- 'tidy_data/activity_subject_mean.csv': Each row represents measurement average value for each activity of each subject
+
+Features descriptions can be found at 'CodeBook.txt'
+
+##Preparation process
+
+Preparation script can be found at 'run_analisys.R' with appropriate comments.
+
+
+Following preparation process was perormed:
+ 
+ 1) load_data function called for test and train data.
+ This function provides:
+  
+    - loads source data sets from file using fread function
+    - assign appropriete features names
+    - remove duplicated columns presented in source data.
+    - adds subject column with subject ID
+    - adds activity_id column for each observation
+    - returnes dplyr data.table
+  
+  2) Bind rows of Test and Train data.frames 
+  
+  3) Select Columns containing mean and sd data for each feature present
+  
+  4) Replace acitivity numeric column with a descriptive data for each activity
+  
+  5) Prepare a special data set with mean values for each activity of each subject
+  
+  6) write prepared data sets into csv files
+  
+
+    
---
+++ run_analysis.R
@@ -0,0 +1,88 @@
+library(dplyr)
+library(tidyr)
+library(data.table)
+
+#Loads data in a Data.Table
+# Adding features names from feature_source
+# Adding subject columt from subject_source
+# Adding activity coulmn from activity source
+
+load_data<-function(features_names='',data_source='',subject_source='',activity_source=''){
+  #Load feature names of the data set
+  features<-tbl_df(fread(features_names, sep=' ',col.names=c('id','feature_name')))
+  
+  #Load TEST Data
+  
+  ##Load measurements data
+  df=tbl_df(fread(data_source,sep=' '))
+  
+  ##Add Features names to data
+  names(df)<-features$feature_name
+  
+  ##Remove duplicated features
+  df<-df[,!duplicated(names(df))]
+  
+  ##Add subject data
+  subject_data<-fread(subject_source,col.names = 'subject')
+  df<-mutate(df,subject=subject_data$subject)
+  
+  ##Activity label train data
+  activity_data<-fread(activity_source,col.names = 'activity_id')
+  df<-mutate(df,activity_id=activity_data$activity_id)
+  
+  df
+}
+
+#Writes Created Tidy Data Set
+write_tidy_data_set<-function(df,path='tydy_data/tidy_data_set.csv'){
+  write.csv(df,path,quote=FALSE, sep=',',row.names = FALSE)
+}
+
+#Load TEST Data
+features_names='data_source/features.txt'
+data_source='data_source/test/X_test.txt'
+subject_source='data_source/test/subject_test.txt'
+activity_source='data_source/test/y_test.txt'
+
+test_data<-load_data(features_names,data_source,subject_source,activity_source)
+
+#Load TRAIN Data
+features_names='data_source/features.txt'
+data_source='data_source/train/X_train.txt'
+subject_source='data_source/train/subject_train.txt'
+activity_source='data_source/train/y_train.txt'
+
+train_data<-load_data(features_names,data_source,subject_source,activity_source)
+
+create_tidy_data_set<-function()
+
+#Merge Train And Test
+all_data<-bind_rows(test_data,train_data)
+
+#Extract only Mean and Standard Deviation columns
+
+##Creates TidyDataSet structure with Activity, Subject, Means and STD of each feature 
+tidy_set <- select(all_data,activity_id,subject,subject,matches('-(mean|std)\\(\\)'))
+
+#Activity put activity label instead of numbers.
+# Read activity labels
+activity_labels<-tbl_df(read.csv('data_source/activity_labels.txt',sep=' ', header = FALSE, col.names = c('activity_id','activity')))
+tidy_set<-tbl_df(merge(tidy_set,activity_labels))
+tidy_set<-select(tidy_set,-activity_id)
+
+#Prettify column names
+names(tidy_set)<-gsub('^t','',names(tidy_set))
+names(tidy_set)<-gsub('^f','Furie',names(tidy_set))
+names(tidy_set)<-gsub('\\(\\)','',names(tidy_set))
+
+#Create Activity-Subject Mean Data set
+by_activity_and_subject<-group_by(tidy_set,activity,subject)
+tidy_mean_by_activity_and_subject<-summarise_each(by_acivity_and_subject,funs(mean))
+
+#Write datasets files
+write_tidy_data_set(tidy_set,'tidy_data/tidy_set.csv')
+write_tidy_data_set(tidy_mean_by_activity_and_subject,'tidy_data/acitivity_subject_mean.csv')
+
+
+
+
